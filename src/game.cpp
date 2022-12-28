34fa942efad4a980281fce02c408b7e668e356f1
Timo Früh <timo.frueh@icloud.com>
2022-12-28 14:37:38 +0100

    refactoring

--- src/game.cpp
+++ src/game.cpp
@@ -32,35 +32,11 @@ void chosen::Game::gameloop() {
 
         cstr::to_lower(command);
 
-        if (cprs::isCommand(command, "north")) {
-            tui.tuiPrint("cmd: move north\n");
-        }
-        else if (cprs::isCommand(command, "east")) {
-            tui.tuiPrint("cmd: move east\n");
-        }
-        else if (cprs::isCommand(command, "south")) {
-            tui.tuiPrint("cmd: move south\n");
-        }
-        else if (cprs::isCommand(command, "west")) {
-            tui.tuiPrint("cmd: move west\n");
-        }
-        else if (cprs::isCommand(command, "up")) {
-            tui.tuiPrint("cmd: move up\n");
-        }
-        else if (cprs::isCommand(command, "down")) {
-            tui.tuiPrint("cmd: move down\n");
-        }
-        else if (cprs::isCommand(command, "look")) {
-            tui.tuiPrint("cmd: look around\n");
-        }
-        else if (cprs::isCommand(command, "talk to")) {
+        if (cprs::isCommand(command, "talk to")) {
             tui.tuiPrint("cmd: talk to someone\n");
         }
         else if (cprs::isCommand(command, "talk")) {
             tui.tuiPrint("cmd: talk to someone (short version)\n");
-        }
-        else if (cprs::isCommand(command, "inventory")) {
-            tui.tuiPrint("cmd: open your inventory\n");
         }
         else if (cprs::isCommand(command, "fight")) {
             tui.tuiPrint("cmd: fight someone\n");
@@ -86,11 +62,35 @@ void chosen::Game::gameloop() {
         else if (cprs::isCommand(command, "unlock")) {
             tui.tuiPrint("cmd: unlock a door\n");
         }
-        else if (cprs::isCommand(command, "scream")) {
+        else if (command == "north") {
+            tui.tuiPrint("cmd: move north\n");
+        }
+        else if (command == "east") {
+            tui.tuiPrint("cmd: move east\n");
+        }
+        else if (command == "south") {
+            tui.tuiPrint("cmd: move south\n");
+        }
+        else if (command == "west") {
+            tui.tuiPrint("cmd: move west\n");
+        }
+        else if (command == "up") {
+            tui.tuiPrint("cmd: move up\n");
+        }
+        else if (command == "down") {
+            tui.tuiPrint("cmd: move down\n");
+        }
+        else if (command == "scream") {
             tui.tuiPrint("cmd: scream (stop it ... get some help)\n");
         }
-        else if (cprs::isCommand(command, "help")) {
+        else if (command == "help") {
             tui.tuiPrint("cmd: help\n");
+        }
+        else if (command == "inventory" || command == "i") {
+            tui.tuiPrint("cmd: open your inventory\n");
+        }
+        else if (command == "look" || command == "look around" || command == "l") {
+            tui.tuiPrint("cmd: look around\n");
         }
         else if (command == "exit" || command == "quit") {
             std::string affirm = tui.tuiInput("Do you really wish to leave the game? (y is affermative)\n");
