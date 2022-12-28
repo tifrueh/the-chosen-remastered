4fe2829bf279a7ade74dca2fd6a9dba20e4214db
Timo Früh <timo.frueh@icloud.com>
2022-12-28 15:18:36 +0100

    add some resources for testing

--- include/resources.hpp
+++ include/resources.hpp
@@ -9,5 +9,6 @@
 #include <array>
 
 namespace crsrc {
-
+    std::string air_demon_kill_txt = "You can't reach the demon, as it is flying high above your head. It might have been beneficial to use a weapon with more range. The demon reaches out with its long claws plunges them into your chest.\nYou die ...";
+    std::string cellar_txt = "You are in a damp cellar. It's cold, the walls are all made of grey rock, and it's generally rather unpleasant here. The only light is coming from a torch mounted to the north wall. You can also see the decaying body of a long dead ... thing? (it is definitely not human) chained to the east wall. That must be where the unbearable stench is coming from.";
 }
