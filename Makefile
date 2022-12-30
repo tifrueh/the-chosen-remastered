4a0dfba98d79bc23f29f7a24e8aa722b679c3458
Timo Früh <timo.frueh@icloud.com>
2022-12-30 12:19:26 +0100

    add player class

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SRCS = src/main.cpp src/tui.cpp src/customstring.cpp src/gameentity.cpp src/parser.cpp src/game.cpp src/world.cpp
+SRCS = src/main.cpp src/tui.cpp src/customstring.cpp src/gameentity.cpp src/parser.cpp src/game.cpp src/world.cpp src/player.cpp
 OBJS = $(subst src,build,$(subst .cpp,.o,$(SRCS)))
 
 CXX ?= g++
---
+++ src/player.cpp
@@ -0,0 +1,31 @@
+// the-chosen-remastered: A short ZORK-like text adventure
+// Copyright (C) 2022  Timo Früh
+// Full copyright notice in main.cpp
+
+
+#include "world.hpp"
+#include "player.hpp"
+
+chosen::Player::Player() : GameEntity("player", "Adventurer", "GameEntity:Player") {
+    location = NULL;
+}
+
+void chosen::Player::setName(const std::string &name) {
+    this->name = name;
+}
+
+void chosen::Player::setLocation(chosen::Room &room) {
+    location = &room;
+}
+
+std::string chosen::Player::getLocationName() {
+    return location->getName();
+}
+
+std::string chosen::Player::getLocationDescription() {
+    return location->getDescription();
+}
+
+std::array<std::string, 3> chosen::Player::getFullLocationDescription() {
+    return location->getFullDescription();
+}
