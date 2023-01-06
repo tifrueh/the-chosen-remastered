ac1e32e687bf4743323621a54b038f410828deee
Timo Früh <timo.frueh@icloud.com>
2023-01-07 00:16:16 +0100

    fix short room description

--- src/world.cpp
+++ src/world.cpp
@@ -105,12 +105,16 @@ std::vector<std::string> chosen::Room::getShortDescription() {
     out.push_back(name);
     out.push_back(std::string(name.size(), '-'));
 
+    if (items.size() > 0 || characters.size() > 0) {
+        out.push_back("");
+    }
+
     for (chosen::Item *item : items) {
-        out.push_back(item->getDescription());
+        out.back() += item->getDescription() + " ";
     }
 
     for (chosen::Character *character : characters) {
-        out.back() += " " + character->getDescription();
+        out.back() += character->getDescription() + " ";
     }
 
     out.push_back(getDoorString());
