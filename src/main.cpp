1f6736789bf24fd06ea301b2207393560d5aa304
Timo Früh <timo.frueh@icloud.com>
2023-04-16 20:53:22 +0200

    include cstring for cli handling

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <algorithm>
 #include "resources.hpp"
 #include "cli.hpp"
